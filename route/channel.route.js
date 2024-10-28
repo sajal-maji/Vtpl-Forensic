64fb8e4b2adeb842e0df6429af3537e07224fc47
tapan <tapan.barik@bluehorse.in>
2024-10-28 16:29:59 +0530

    update for pointer and channel

--- route/channel.route.js
+++ route/channel.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 
-const { grayscaleConversion,colorswitchConversion,colorConversion,extractSingleChannel } = require('../controller/channel.controller');
+const { grayscaleConversion,colorswitchConversion,colorConversion,extractSingleChannel,displaySelectedChannels} = require('../controller/channel.controller');
 
 
 router.put('/grayscale-conversion', grayscaleConversion);
@@ -10,5 +10,17 @@ router.put('/color-conversion', colorConversion);
 router.put('/extract-single-channel', extractSingleChannel);
 router.put('/display-selected-channels', displaySelectedChannels);
 // router.put('/preview-filter', previewFilter);ColorConversionFilter
+
+
+
+const { levelControl,brightnessContrast,contrastStretch,clahe,intensityChange} = require('../controller/adjust.controller');
+router.put('/level-control', levelControl);
+router.put('/brightness-contrast', brightnessContrast);
+router.put('/contrast-stretch', contrastStretch);
+router.put('/clahe', clahe);
+router.put('/intensity-change', intensityChange);
+// router.put('/brightness-contrast', contrastStretch);
+// router.put('/brightness-contrast', contrastStretch);
+
  
 module.exports = router;
