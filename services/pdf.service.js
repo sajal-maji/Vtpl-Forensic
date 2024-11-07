2c2e961e5b62afb9b76209de7efd6673f2c3c087
biswajitpalkabi.bluehorse@gmail.com <biswajitpalkabi.bluehorse@gmail.com>
2024-11-07 18:50:18 +0530

    chnges

--- services/pdf.service.js
+++ services/pdf.service.js
@@ -2,23 +2,24 @@ const Imageoperation = require('../model/imageoperation.model');
 const fs = require('fs');
 const path = require('path');
 
-const getOperationDetails = async (projectId) => {
+const getOperationDetails = async (projectId) => {    
     const operationDetails = await Imageoperation.find({ projectId: projectId });
+    
     let processes = [];
     if (operationDetails.length > 0) {
-        for (let i = 1; i <= operationDetails.operationDetails; i++) {
+        for (let i = 0; i < operationDetails.length; i++) {
             let data = {
                 "process_index": operationDetails[i].processIndex,
                 "process_name": operationDetails[i].processName,
                 "exe_details_avail_flag": operationDetails[i].exeDetailsAvailFlag,
                 "exe_details": {
-                    "details": operationDetails[i].exeDetails
+                    "details": JSON.parse(operationDetails[i].exeDetails)
                 }
             }
             processes.push(data);
         }
-    }
-    return processes
+    }    
+    return processes;
 };
 
 module.exports = {
